6a92058a2690cea77c5e435535def90f4de7e210
Dilushika <dilushika@dilushikaw.local>
2016-09-22 17:58:11 +0530

    Added Testing for http/post

--- jasmine/spec/customTestingFunc.js
+++ jasmine/spec/customTestingFunc.js
@@ -1,39 +1,60 @@
-describe("MyApp",function() {
+	describe("MyApp",function() {
 
-	beforeEach(module('mainApp'));
+		beforeEach(module('mainApp'));
 
-	describe('Testing the get function',function(){
+		describe('Testing the angular controllers',function(){
 
-		var httpBackend,controller, scope, httpp;
+			var httpBackend,controller, scope, httpp, value1, value2, value3, todos;
+
+			todos = [{ data:'value1'}, {data:'value2' }];
+
+			beforeEach(inject(function($rootScope, $controller, $httpBackend, $http) {
+
+				scope = $rootScope.$new();
+				httpBackend = $httpBackend;
+				httpp = $http;
+				
+				
+				httpBackend.when("GET", "/mytodos").respond(200,{todos});
+				controller = $controller;
+
+			}));
 
 
-		beforeEach(inject(function($rootScope, $controller, $httpBackend, $http) {
+			it('Should get all da data when loading the page',function() {
+				
+				controller('app', {
+					$scope: scope,
+					$http: httpp
+				});
 
-			scope = $rootScope.$new();
-			httpBackend = $httpBackend;
-			httpp = $http;
-			controller = $controller;
-			
+				httpBackend.flush();
 
-		}));
+				expect(scope.todolist).toEqual({todos});
 
-
-		it('Should get all da data when loading the page',function() {
-			var scope = {};
-
-			httpBackend.when("GET", "/mytodos").respond(200,{ data: 'value' });
-
-			controller('app', {
-				$scope: scope,
-				$http: httpp
 			});
 
-			httpBackend.flush();
 
-			expect(scope.todolist).toEqual({ data: 'value' });
+			it('Should post all mytodos',function(){
+				
+				httpBackend.when("POST", "/mytodos").respond(200,{todos});
+				
+				controller('app', {
+					$scope: scope,
+					$http: httpp
+				});
+				scope.task = 'hello';
+							
+				scope.addTasks();
 
-		});
+				httpBackend.flush();
 
-	});	
 
-});
+				expect(scope.todolist).toEqual({todos});
+
+				expect(scope.task).toEqual(null);
+
+			});
+
+		});	
+	});
